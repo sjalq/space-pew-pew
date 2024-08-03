f24695a55872e2339fac8e4f6f41c8f41248147c
sjalq <schalk.dormehl@gmail.com>
2024-08-03 22:05:38 +0200

    intialize pew pew!

---
+++ src/Types.elm
@@ -0,0 +1,92 @@
+module Types exposing (..)
+
+import Browser exposing (UrlRequest)
+import Browser.Navigation exposing (Key)
+import Time
+import Url exposing (Url)
+
+
+type alias FrontendModel =
+    { key : Key
+    , message : String
+    }
+
+
+type alias BackendModel =
+    { message : String
+    }
+
+
+type FrontendMsg
+    = UrlClicked UrlRequest
+    | UrlChanged Url
+    | NoOpFrontendMsg
+
+
+type ToBackend
+    = NoOpToBackend
+
+
+type BackendMsg
+    = NoOpBackendMsg
+
+
+type ToFrontend
+    = NoOpToFrontend
+
+
+
+-- Game Types for Space Pew Pew!
+
+
+type alias Ship =
+    { position : Vector2D
+    , velocity : Vector2D
+    , rotation : Float
+    , crew : Int
+    , energy : Int
+    , shipType : ShipType
+    }
+
+
+type ShipType
+    = Triangle
+
+
+type alias Projectile =
+    { position : Vector2D
+    , velocity : Vector2D
+    , damage : Int
+    , lifetime : Float
+    }
+
+
+type alias Planet =
+    { position : Vector2D
+    , radius : Float
+    , gravity : Float
+    }
+
+
+type alias Vector2D =
+    { x : Float
+    , y : Float
+    }
+
+
+type alias GameState =
+    { ships : List Ship
+    , projectiles : List Projectile
+    , planet : Planet
+    , timeElapsed : Float
+    }
+
+
+
+-- Game Loop
+
+
+type GameMsg
+    = Tick Time.Posix
+    | FireProjectile Ship
+    | MoveShip Ship Vector2D
